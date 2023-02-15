9bfee36daf225c78535f52c0a1b17e2337390fea
Dieguidev <diegogaraycullas@gmail.com>
2023-02-14 22:59:54 -0500

    segunda entrega

--- src/pages/Characters/Character.js
+++ src/pages/Characters/Character.js
@@ -3,25 +3,29 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './character.css'
 
 const Character = () => {
-  const [ characters, setcharacters] = useState([])
+  const [characters, setcharacters] = useState([])
 
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/character')
-      .then(res =>setcharacters(res.data.results));
-  },[])
+      .then(res => setcharacters(res.data.results));
+  }, [])
 
   console.log(characters)
   return (
     <div>
       <h1>Characters</h1>
-      <ul>
+      <div>
         {characters?.map(character => (
-          <p key={character.id}><Link className='link' to={`/characters/${character.id}`}>{character.name}</Link></p>
-          
+          <Link className='link' to={`/characters/${character.id}`}>
+            <p className='targetLink' key={character.id}>{character.name}</p>
+            <img className='img-characters' src={character.image} alt="" />
+          </Link>
+
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
